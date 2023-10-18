6c10d4a3d07e4cc33150a4bbad035466e826917f
p3rAsperaAdAstra <60529579+p3rAsperaAdAstra@users.noreply.github.com>
2023-10-18 14:01:58 +0200

    v1.1
    little addition for the future.

--- pymodules.bat
+++ pymodules.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal enabledelayedexpansion
 
+REM At the time of the initial commit (18.10.2023) the python version where all the packages are compatible was python 3.9
 REM List of Python libraries to install
 set "modules=matplotlib scipy numpy pandas lxml requests rdkit sympy"
 
